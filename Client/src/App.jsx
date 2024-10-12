76b2eba207f640e8d782e689506bcd45d3533622
KClower <kclower3@gmail.com>
2024-10-12 15:32:15 -0500

    deleted code that was not needed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,13 +4,8 @@ import { NavBar } from './Components/NavBar';
 import { Home } from './pages/Home';
 import SignIn from './pages/SignIn';
 import Register from "./pages/Register";
-// import WorkoutList from './Components/WorkoutList';
-// import WorkoutSignup from './Components/WorkoutSignup';
 import { InstructorDashboard } from './pages/InstructorDashboard';
 import { ClientDashboard } from './pages/ClientDashboard';
-// import CreateClassForm from './Components/ClassForms/CreateClassForm';
-// import UpdateClassForm from './Components/ClassForms/UpdateClassForm';
-// import ThankYou from './pages/ThankYouPage';
 
 
 
@@ -31,13 +26,9 @@ function App() {
         <Route exact path="/" Component={Home} />
         <Route path="/SignIn" Component={SignIn} />
         <Route path="/Register" Component={Register} />
-        {/* <Route path="/WorkoutList" Component={WorkoutList} /> */}
-        {/* <Route path="/WorkoutSignup/:id" Component={WorkoutSignup} /> */}
         <Route path="/Instructor/dashboard" Component={InstructorDashboard} />
         <Route path="/Client/dashboard" Component={ClientDashboard} />
-        {/* <Route path="/CreateClassForm" Component={CreateClassForm} />
-        <Route path="/UpdateClassForm" Component={UpdateClassForm} />
-        <Route path="/ThankYouPage" Component={ThankYou} /> */}
+
       </Routes>
 
 
